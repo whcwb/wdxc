3e490bdf9a9761344a9e6e018e124011dfb8886c
slu <384920400@qq.com>
2019-05-30 17:41:09 +0800

    形成补偿

--- dao/dao-biz/src/main/java/com/ldz/dao/biz/mapper/ClGpsLsMapper.java
+++ dao/dao-biz/src/main/java/com/ldz/dao/biz/mapper/ClGpsLsMapper.java
@@ -3,6 +3,7 @@ package com.ldz.dao.biz.mapper;
 import com.ldz.dao.biz.bean.gpsSJInfo;
 import com.ldz.dao.biz.model.ClGpsLs;
 import com.ldz.util.mapperprovider.InsertListMapper;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
@@ -22,4 +23,7 @@ public interface ClGpsLsMapper extends Mapper<ClGpsLs>, InsertListMapper<ClGpsLs
     Map<String, Double> getMSpeedAndAvgSpeed(@Param("zdbh") String zdbh,@Param("startTime") String startTime,@Param("endTime") String endTime);
 	@Select(" select bdjd , bdwd from cl_gps_ls where cjsj >= #{cjsj} and zdbh = #{zdbh} order by cjsj asc")
     List<Map<String, BigDecimal>> getJdAndWd(@Param("zdbh") String zdbh, @Param("cjsj") Date cjsj);
+
+	@Delete( " delete from CL_GPS_LS where ZDBH = #{zdbh} and cjsj >=#{sTime} and cjsj <= #{eTime} " )
+	void delGps(@Param("zdbh") String zdbh, @Param("sTime") Date sTime,@Param("eTime") Date endTime);
 }
